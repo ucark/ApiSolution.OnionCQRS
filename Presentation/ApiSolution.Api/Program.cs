2ed84815ba5cf82d0da89d244021bfb7c4e5c8f5
ucark <ucarkudret7@gmail.com>
2024-11-29 15:38:11 +0300

    appsetting.json güncellemeleri

--- Presentation/ApiSolution.Api/Program.cs
+++ Presentation/ApiSolution.Api/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var env = builder.Environment;
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
