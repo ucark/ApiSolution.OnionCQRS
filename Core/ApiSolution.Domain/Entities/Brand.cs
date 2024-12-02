6d04976c89d574b952fb48217e16c258f60db54c
ucark <ucarkudret7@gmail.com>
2024-12-02 14:41:55 +0300

    Entity tabloları ve ilişkisel yapılar kuruldu

---
+++ Core/ApiSolution.Domain/Entities/Brand.cs
@@ -0,0 +1,23 @@
+﻿using ApiSolution.Domain.Common;
+using ApiSolution.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSolution.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/ApiSolution.Domain/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using ApiSolution.Domain.Common;
+using ApiSolution.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSolution.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+            
+        }
+        public Category(int parentid, string name, int priorty)
+        {
+            ParentId = parentid;
+            Name = name;
+            Priorty = priorty;
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
